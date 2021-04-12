421963663abcbac4590e393bd887d761651022e3
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-04-11 20:50:30 -0400

    Installed react-social-icons and applied icons and styling to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,63 @@
 import React from "react";
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
 
 export default function NavBar() {
-    return (
-        <header>
-            <div>
-                <nav>
-                    <NavLink to="/" exact>
-                        Jason McGinthy
-                    </NavLink>
-                    <NavLink to="/post">
-                        Blog Posts
-                    </NavLink>
-                    <NavLink to="/project">
-                        Projects
-                    </NavLink>
-                    <NavLink to="/about">
-                        About Me!
-                    </NavLink>
-                </nav>
-            </div>
-        </header>
-    )
+  return (
+    <header className="bg-red-600">
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex">
+          <NavLink
+            to="/"
+            exact
+            activeClassName="text-white"
+            className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
+          >
+            Jason McGinthy
+          </NavLink>
+          <NavLink
+            to="/post"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+          >
+            Blog Posts
+          </NavLink>
+          <NavLink
+            to="/project"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            to="/about"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+          >
+            About Me!
+          </NavLink>
+        </nav>
+        <div className="inline-flex py-3 px-3 my-6">
+          <SocialIcon
+            url="https://www.linkedin.com/in/jasonmcginthy/"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://github.com/codragon2020"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://twitter.com/jasonmcginthy?lang=en"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+        </div>
+      </div>
+    </header>
+  );
 }

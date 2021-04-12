59b3755cede2f4475e0d67c870a6857ddce9e234
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-04-11 21:23:37 -0400

    Styled Home page.  Added some new backgrounds. Updated NavBar color to match

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,19 @@
 import React from "react";
+import image from "../green-tech-11.jpg";
 
 export default function Home() {
-    return <h1>Home Page!</h1>
+  return (
+    <main>
+      <img
+        src={image}
+        alt="Tech Background"
+        className="absolute object-cover w-full h-full"
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-green-900 font-bold cursive leading-none lg:leading-snug home-name">
+          Hello! I'm Jason.
+        </h1>
+      </section>
+    </main>
+  );
 }

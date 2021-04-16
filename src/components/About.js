e749773d053e0c4aee157ccc48c429ce7e5a7024
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-04-16 07:06:57 -0400

    Adjusting css in NavBar and About to help with mobile responsiveness

--- src/components/About.js
+++ src/components/About.js
@@ -24,15 +24,16 @@ export default function About() {
     if (!author) return <div>Loading...</div>;
     
     return (
-        <main className="relative">
-            <img src={greenai} alt="Green Tech Pic" className="absolute w-full" />
-            <div className="p-10 lg:pt-48 container mx-auto relative">
-                <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
+        <main className="bg-green-100 relative">
+            {/* <img src={greenai} alt="Green Tech Pic" className="absolute w-full h-full" /> */}
+            <div className="p-10 lg:pt-48 sm:pt-12 container mx-auto relative">
+                <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-5">
                     <img src={urlFor(author.authorImage).url()} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8" alt={author.name} />
                     <div className="text-lg flex flex-col justify-center">
-                        <h1 className="cursive text-6xl text-green-300 mb-4">
-                            Hey there. I'm{" "}
+                        <h1 className="cursive lg:text-6xl text-green-300 sm:text-3xl mb-4">
+                            Hey there. Welcome to{" "}
                             <span className="text-green-100">{author.name}</span>
+                            's webpage!
                         </h1>
                         <div className="prose lg:prose-xl text-white">
                             <BlockContent blocks={author.bio} projectId="ogu7ciby" dataset="production" />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ export default function NavBar({ fixed }) {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
     <>
-      <nav className="bg-green-600 relative flex flex-wrap items-center justify-between px-2 py-3 mb-3">
+      <nav className="bg-green-600 relative flex flex-wrap items-center justify-between px-2 py-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <NavLink
@@ -28,7 +28,7 @@ export default function NavBar({ fixed }) {
           <div
             className={
               "lg:flex flex-grow items-center" +
-              (navbarOpen ? " flex" : " hidden")
+              (!navbarOpen ? " flex" : " hidden")
             }
             id="example-navbar-danger"
           >

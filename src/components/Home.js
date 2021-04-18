d9674d1761a9fe6ecc527e1fd8d3544ec68c0a69
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-04-18 10:44:50 -0400

    Home page screen size adjustments for styling and pics

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import image from "../images/green-tech-11.jpg";
+import portImage from "../images/portImage.jpg";
 
 export default function Home() {
+  const [clientWidth, setclientWidth] = useState(document.body.clientWidth)
+  useEffect(() => {
+    const changeBackground = () => {
+      console.log(document.body.clientWidth);
+
+      setclientWidth(document.body.clientWidth)
+    } 
+    window.addEventListener("resize", changeBackground);
+
+  }, [])
   return (
     <main>
       <img
-        src={image}
+        src={clientWidth > 1159 ? image : portImage}
         alt="Tech Background"
-        className="absolute object-cover w-full h-full lg:pt-0"
+        className="absolute object-cover w-full h-full"
       />
       <section className="relative flex justify-center min-h-screen pt-12 pt-64 px-30">
-        <h1 className="lg:text-9xl text-6xl text-green-900 font-bold cursive leading-none lg:leading-snug text-center">
+        <h1
+          className={
+            clientWidth > 1159
+              ? "lg:text-9xl text-6xl font-bold leading-none cursive lg:leading-snug text-center text-green-900"
+              : "lg:text-9xl text-6xl font-bold leading-none cursive lg:leading-snug text-center text-white"
+          }
+        >
           Hello! Welcome to my site.
         </h1>
       </section>

d5372f2f64c31bff58674f9c055d11be1153ca5c
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-04-19 07:12:30 -0400

    Added Contact information to Contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,29 @@
 import React from "react";
 
 export default function Contact() {
-    return <h1>This is my contact page</h1>
+  return (
+    <main className="bg-green-100 min-h-screen p-12">
+      <section className="container mx-auto">
+        <h1 className="text-5xl flex justify-center cursive">
+          Jason McGinthy's <br></br>Contact Information
+        </h1>
+        <br></br>
+        <div className="lg:flex flex-grow items-center">
+          <ul className="flex flex-col text-2xl ">
+            <li>Email: jasonmcginthy@gmail.com</li>
+            <li>Phone: 804-389-0908</li>
+            <li className="text-green-600 hover:text-blue-800">
+              <a
+                href="https://drive.google.com/file/d/1VU7L00X-2CwBd74S3RpmNdkKifcUPNGg/view?usp=sharing"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Resume
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </main>
+  );
 }
